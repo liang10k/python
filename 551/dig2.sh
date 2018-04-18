0ded4ac2e59f629e95770e18c289e709f5f79d00
liang10k <35902409+liang10k@users.noreply.github.com>
2018-04-17 23:11:26 -0400

    Add files via upload

---
+++ 551/dig2.sh
@@ -0,0 +1,30 @@
+URL=www.renren.com
+n=0
+echo  ${URL} > $URL.txt
+echo "dig ${URL} begin now please wait"
+for i in {1..86400}
+do
+	sleep 1
+	res=`dig +norecurse +noall +answer ${URL}`
+	if [ -z "$res" ]
+	then
+		n=$[$n+1]
+	else
+		if [ $n -ne 0 ] 
+	    # if n != 0
+		then
+			echo  $n  >> $URL.txt
+			n=0;
+		fi
+	fi
+	if [ $[$i%9999] == 0 ]
+	# set the end of the fig program
+	then
+		echo $i
+	fi
+done
+if [ $n -ne 0 ]
+then
+	echo  $n  >> $URL.txt
+fi
+FINISH
